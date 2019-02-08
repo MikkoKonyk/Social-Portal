d38e9a27a0f0b92f90cf1f9b44af51cc5d387b3f
Michael <mkonyk@noltic.com>
2019-02-08 14:59:34 +0200

    scripts.js - Code reformat

--- staticresources/WeatherStaticResources/js/scripts.js
+++ staticresources/WeatherStaticResources/js/scripts.js
@@ -1,32 +1,37 @@
-        function getWeather() {
-            var city = document.getElementById('city').value;
-            var countryCode = document.getElementById('countryCode').value;
-            SP_WeatherCtrl.getWeatherByCity(
-				city,  
-                countryCode,
-            function (result, event) { 
-                    if (event.status) { 
-                        document.getElementById('temperatureId').innerHTML = result.temp;
-                        document.getElementById('humidityId').innerHTML =  result.humidity;
-                        document.getElementById('pressureId').innerHTML =  result.pressure;
-                    } else { 
-                    }
-                }, { buffer: true, escape: true, timeout: 120000 });
-        };
+function getWeather() {
+    var city = document.getElementById('city').value;
+    var countryCode = document.getElementById('countryCode').value;
+    SP_WeatherCtrl.getWeatherByCity(
+		city,  
+		countryCode,
+    	function (result, event) { 
+			if (event.status) { 
+				document.getElementById('temperatureId').innerHTML = result.temp;
+				document.getElementById('humidityId').innerHTML =  result.humidity;
+				document.getElementById('pressureId').innerHTML =  result.pressure;
+			} else {
+			}
+     	}, 
+    	{ buffer: true, escape: true, timeout: 120000 }
+    );
+};
 
-        navigator.geolocation.getCurrentPosition(
-        function getWeatherByIpLocation(position) {
-            var lat = position.coords.latitude;
-            var lon = position.coords.longitude;
-            SP_WeatherCtrl.getWeatherByIpLocation(
-                lat,
-                lon,
-            function (result, event) {
-                    if (event.status) {
-                        document.getElementById('temperatureId').innerHTML = result.temp;
-                        document.getElementById('humidityId').innerHTML =  result.humidity;
-                        document.getElementById('pressureId').innerHTML =  result.pressure;
-                    } else {
-                    }
-                });
-        })
+navigator.geolocation.getCurrentPosition(
+	function getWeatherByIpLocation(position) {
+		var lat = position.coords.latitude;
+		var lon = position.coords.longitude;
+		SP_WeatherCtrl.getWeatherByIpLocation(
+			lat,
+			lon,
+			function (result, event) {
+				if (event.status) {
+					document.getElementById('temperatureId').innerHTML = result.temp;
+					document.getElementById('humidityId').innerHTML =  result.humidity;
+					document.getElementById('pressureId').innerHTML =  result.pressure;
+				} else {
+				}
+			},
+			{ buffer: true, escape: true, timeout: 120000 }
+		);
+	}
+);
