2b39322f5cc498e5daa78300ff3a96d19ce58f33
Da239163 <demalviv@gmail.com>
2019-02-09 13:51:32 +0200

    weather widget was updated

--- staticresources/WeatherStaticResources/js/scripts.js
+++ staticresources/WeatherStaticResources/js/scripts.js
@@ -5,10 +5,11 @@ function getWeather() {
 		city,  
 		countryCode,
     	function (result, event) { 
-			if (event.status) { 
-				document.getElementById('temperatureId').innerHTML = result.temp;
-				document.getElementById('humidityId').innerHTML =  result.humidity;
-				document.getElementById('pressureId').innerHTML =  result.pressure;
+			if (event.status) {
+			    document.getElementById('message').innerHTML = 'The weather parameter in selected by you city now are:';
+				document.getElementById('temperatureId').innerHTML = result.temp + 'ºC';
+				document.getElementById('humidityId').innerHTML =  result.humidity + '%';
+				document.getElementById('pressureId').innerHTML =  result.pressure + 'mb';
 			} else {
 			}
      	}, 
@@ -25,9 +26,10 @@ navigator.geolocation.getCurrentPosition(
 			lon,
 			function (result, event) {
 				if (event.status) {
-					document.getElementById('temperatureId').innerHTML = result.temp;
-					document.getElementById('humidityId').innerHTML =  result.humidity;
-					document.getElementById('pressureId').innerHTML =  result.pressure;
+				    document.getElementById('message').innerHTML = 'The weather parameter in your city now are:';
+					document.getElementById('temperatureId').innerHTML = result.temp + 'ºC';
+					document.getElementById('humidityId').innerHTML =  result.humidity + '%';
+					document.getElementById('pressureId').innerHTML =  result.pressure + 'mb';
 				} else {
 				}
 			},
