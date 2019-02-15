16a60eddcfbdd8aa265a654e493d6d7352ebff60
Da239163 <demalviv@gmail.com>
2019-02-15 18:12:25 +0200

    weather widget updated for different browsers

--- staticresources/WeatherStaticResources/js/scripts.js
+++ staticresources/WeatherStaticResources/js/scripts.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', getGeolocation, false);
+
 function getWeather() {
     var city = document.getElementById('city').value;
     var countryCode = document.getElementById('countryCode').value;
@@ -17,23 +19,36 @@ function getWeather() {
     );
 };
 
-navigator.geolocation.getCurrentPosition(
-	function getWeatherByIpLocation(position) {
-		var lat = position.coords.latitude;
-		var lon = position.coords.longitude;
-		SP_WeatherCtrl.getWeatherByIpLocation(
-			lat,
-			lon,
-			function (result, event) {
-				if (event.status) {
-				    document.getElementById('message').innerHTML = 'The weather parameter in your city now are:';
-					document.getElementById('temperatureId').innerHTML = result.temp + 'ºC';
-					document.getElementById('humidityId').innerHTML =  result.humidity + '%';
-					document.getElementById('pressureId').innerHTML =  result.pressure + 'mb';
-				} else {
-				}
-			},
-			{ buffer: true, escape: true, timeout: 120000 }
-		);
-	}
-);
+function getGeolocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showLocation, showError);
+    } else {
+        alert('Geolocation is not supported by this browser.');
+    }
+};
+
+function showLocation(position) {
+    console.log('lat: ' + position.coords.latitude);
+    console.log('lon: ' + position.coords.longitude);
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    SP_WeatherCtrl.getWeatherByIpLocation(
+        lat,
+        lon,
+        function (result, event) {
+            if (event.status) {
+                document.getElementById('message').innerHTML = 'The weather parameter in your city now are:';
+                document.getElementById('temperatureId').innerHTML = result.temp + 'ºC';
+                document.getElementById('humidityId').innerHTML =  result.humidity + '%';
+                document.getElementById('pressureId').innerHTML =  result.pressure + 'mb';
+            } else {
+            }
+        },
+        { buffer: true, escape: true, timeout: 120000 }
+    );
+};
+
+function showError(error) {
+    console.log( 'getCurrentPosition returned error', error);
+};
+
