05d058e88d63406b8ddf4c61a21e7cf01a145958
Andrii Demchuk <demalviv@gmail.com>
2019-04-05 15:51:34 +0300

    task2: added one more method for factorial

--- Factorial.java
+++ Factorial.java
@@ -8,12 +8,24 @@ public class Factorial {
         System.out.println("Add a number:");
         if (scanner.hasNextInt()) {
             number = scanner.nextInt();
-                for(int i=number; i>0; i--){
-                    result*=i;
-                }
-                System.out.println("Factorial of number " + number + " = " + result);
+            for (int i = number; i > 0; i--) {
+                result *= i;
+            }
+            System.out.println("Factorial of number " + number + " = " + result);
         } else {
             System.out.println("Error! You added not number!");
+        }
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Add a number for a second variant:");
+        System.out.println("Factorial of from second number = " + factorial(scanner2.nextInt()));
+    }
+
+    public static int factorial(int number) {
+        if (number == 0) {
+            return 1;
+        } else {
+            return (number * factorial(number - 1));
         }
     }
 }
