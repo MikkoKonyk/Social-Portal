27cd86f71c8adde2aac04119b787cb2b849b7e24
Andrii Demchuk <demalviv@gmail.com>
2019-04-05 13:50:16 +0300

    initial project version

---
+++ Factorial.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Factorial {
+    public static void main(String[] args) {
+        int number;
+        long result = 1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Add a number:");
+        if (scanner.hasNextInt()) {
+            number = scanner.nextInt();
+                for(int i=number; i>0; i--){
+                    result*=i;
+                }
+                System.out.println("Factorial of number " + number + " equals " + result);
+        } else {
+            System.out.println("Error! You added not number!");
+        }
+    }
+}
---
+++ LettersCount.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LettersCount {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Add a text, please:");
+        String text = in.readLine();
+        Integer count = 0;
+        for (Integer i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == 'a') {
+                count++;
+            }
+        }
+        System.out.println("Count of 'a' letters = " + count);
+    }
+}
