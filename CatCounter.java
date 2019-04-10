9c5d7f217efe7974b42856c939ed6af4353e4d15
Andrii Demchuk <demalviv@gmail.com>
2019-04-10 21:17:21 +0300

    added changes to stringsTask#1

--- CatCounter.java
+++ CatCounter.java
@@ -1,9 +1,7 @@
-import java.util.Scanner;
-
 public class CatCounter {
     public static void main(String[] args) {
-        int count=0;
-        String text="hi cat or cat not cat but cat is cat after";
+        int count = 0;
+        String text = "hi cat or cat not cat but cat is cat after";
         String[] textArray = text.split(" ");
         for(int i=0; i<textArray.length; i++) {
             if (textArray[i].equals("cat")) {
